6943a307e35914d79cebb5e510d788b21771b9d9
Nazar Melnik <115361939+Arlikey@users.noreply.github.com>
2026-04-11 14:32:14 +0300

    Implementation Navbar with some basic GSAP

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+type NavLink = {
+  title: string;
+  id: string;
+};
+
+export const navLinks: NavLink[] = [
+  { id: "cocktails", title: "Cocktails" },
+  { id: "about", title: "About Us" },
+  { id: "work", title: "The Art" },
+  { id: "contact", title: "Contact" },
+];
